f029a60ff32bade7762b47eed14f0ec52eb7d201
Aiska Basnet <aiska.basnet@gmail.com>
2017-12-11 15:19:13 +0545

    Added layout and classes for dialog

---
+++ app/src/main/java/com/aiska/drawing/Drawing.java
@@ -0,0 +1,15 @@
+package com.aiska.drawing;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by aiska on 12/11/17.
+ */
+
+public class Drawing extends View {
+    public Drawing(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
---
+++ app/src/main/java/com/aiska/drawing/LineWidthDialogFragment.java
@@ -0,0 +1,8 @@
+package com.aiska.drawing;
+
+/**
+ * Created by aiska on 12/11/17.
+ */
+
+public class LineWidthDialogFragment {
+}
