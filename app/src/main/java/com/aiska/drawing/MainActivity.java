82302f236df443df063509b4a2a6a47ee3238731
Aiska Basnet <aiska.basnet@gmail.com>
2017-12-11 15:29:51 +0545

    Main Activity

--- app/src/main/java/com/aiska/drawing/MainActivity.java
+++ app/src/main/java/com/aiska/drawing/MainActivity.java
@@ -1,5 +1,7 @@
 package com.aiska.drawing;
 
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -15,7 +17,15 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        int screenSize = getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK;
+        if (screenSize == Configuration.SCREENLAYOUT_SIZE_XLARGE){
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        }
+        else
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
     }
+
 
 
 }
